0bddd2d145a559afac0b8600538ddc292338d13c
Calvin Lee <leecalv@ecs.vuw.ac.nz>
2020-11-15 22:20:00 +1300

    Added Firebase

---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,89 @@
+// import app from 'firebase/'
+import app from 'firebase/app'
+import 'firebase/database';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyD-NAxOrD9fJR11rZPd2xlYL4XyExh1wcA",
+  authDomain: "smartdisplay-de3c6.firebaseapp.com",
+  databaseURL: "https://smartdisplay-de3c6.firebaseio.com",
+  projectId: "smartdisplay-de3c6",
+  storageBucket: "smartdisplay-de3c6.appspot.com",
+  messagingSenderId: "309238368036",
+  appId: "1:309238368036:web:8cd13ca4a63d96a76595b1"
+};
+
+class Firebase {
+
+  ////////////////////////////
+  /*Fields*/
+  ////////////////////////////
+  auth : app.auth.Auth;
+  db: app.database.Database;
+
+  ////////////////////////////
+  /*Constructor*/
+  ////////////////////////////
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(config);
+    }
+
+    this.auth = app.auth();
+    this.db = app.database();
+  }
+
+  ////////////////////////////
+  /*Creating Accounts*/
+  ////////////////////////////
+
+  /**
+   * Creating an Account using Email and Password.
+   * @param email - the string of their email
+   * @param password - the password for their account
+   */
+  doCreateUserWithEmailAndPassword = async (email : string, password : string) => {
+    await this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.currentUser;
+  }
+
+  ////////////////////////////
+  /*Signing In*/
+  ////////////////////////////
+  
+  /**
+   * Signing in with Email and Password.
+   * @param email - the string of their email
+   * @param password - the password for their account
+   */
+  doSignInWithEmailAndPassword = async (email : string, password : string) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  ////////////////////////////
+  /*Misc Functions*/
+  ////////////////////////////
+
+  doSignOut = async () => {
+    this.auth.signOut();
+  }
+
+  doPasswordReset = async (email : string) =>  {
+    this.auth.sendPasswordResetEmail(email);
+  }
+ 
+  doPasswordUpdate = async (password : string) => {
+    if (this.auth.currentUser !== null) {
+      this.auth.currentUser.updatePassword(password);
+    } else {
+      //Attempted to Change password when the current user was not selected/logged in
+    }
+  }
+
+  user = (uid : any) => {
+    return this.db.ref(`users/${uid}`);
+  }
+
+}
+
+export default Firebase;
